dfe91bfa3f2fe7287f77dd8b5d6dcb98960d40c1
cwj <cwj19941219@gmail.com>
2024-12-09 00:59:22 +0900

    test3

---
+++ Editor/PackageInstaller.cs
@@ -0,0 +1,38 @@
+
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class PackageInstaller
+{
+	private static readonly string[] gitUrls = new string[]
+	                                           {
+		                                           UniRxUrl, UniTaskUrl
+	                                           };
+	private const string UniRxUrl = "https://github.com/neuecc/UniRx.git?path=Assets/Plugins/UniRx/Scripts#v7.1.0";
+	private const string UniTaskUrl = "https://github.com/Cysharp/UniTask.git?path=src/UniTask/Assets/Plugins/UniTask#v2.5.10";
+
+	static PackageInstaller()
+	{
+		EditorApplication.update += InstallPackages;
+	}
+
+	private static void InstallPackages()
+	{
+		// 유니티가 실행 중일 때만 동작하도록 설정
+		if (!EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isCompiling)
+		{
+			// 설치 요청
+			Debug.Log("Installing required packages...");
+			foreach (var url in gitUrls)
+			{
+				Client.Add(url);
+			}
+			// 스크립트 등록 해제
+			EditorApplication.update -= InstallPackages;
+		}
+	}
+}
+
